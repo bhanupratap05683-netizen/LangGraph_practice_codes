66869315f24c4fc98d2fd7f820c9e56169dfec61
bhanu_finance_dev <bhanupratap05683@gmail.com>
2026-08-19 13:20:58 +0530

    Edit the file

--- Day 1 practice code.py
+++ Day 1 practice code.py
@@ -1,12 +1,14 @@
 import os
 from langchain.chat_models import init_chat_model
 from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
 from langgraph.checkpoint.memory import MemorySaver
+
 llm = init_chat_model(
     model="qwen2.5-coder:7b",
-    model_provider="ollama",
     temperature=0.0,
+    model_provider="ollama",
     base_url="http://localhost:11434"
 )
-response = llm.invoke("Write a Python function that takes a list of numbers and returns the sum of the even numbers in the list.")
+response = llm.invoke("what is the capital of france?")
 print(response)
