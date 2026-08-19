87ae76255bae819544ea13cfb9de9230877ce74b
bhanu_finance_dev <bhanupratap05683@gmail.com>
2026-08-19 13:52:48 +0530

    Add the recent chat memory

--- Day 3 practice code.py
+++ Day 3 practice code.py
@@ -5,11 +5,15 @@ model = ChatOllama(
     temperature=0.0,    
     base_url="http://localhost:11434"
 )
+messages = [
+]
 print("----------Pucho kya puchna hai?----------")
 while True:
     prompt = input("You: ")
+    messages.append({"role": "user", "content": prompt})
     if prompt.lower() in ["exit", "quit"]:
         print("Chalo bhaag jao! Bye!")
         break
-    response = model.invoke(prompt)
+    response = model.invoke(messages)
+    messages.append({"role": "assistant", "content": response.content})
     print(f"Gyani: {response.content}")    
