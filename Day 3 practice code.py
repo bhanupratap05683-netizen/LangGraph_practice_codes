81256253fd4e99975ff6c21aa06135fd41abd42d
bhanu_finance_dev <bhanupratap05683@gmail.com>
2026-08-19 14:41:15 +0530

    using langchain for short-term memory management

--- Day 3 practice code.py
+++ Day 3 practice code.py
@@ -1,20 +1,21 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 model = ChatOllama(
     model="qwen2.5-coder:7b",
     temperature=0.0,    
     base_url="http://localhost:11434"
 )
-messages = [
+messages = [ SystemMessage(content="You are a Agent that response in funny way."),
 ]
 print("----------Pucho kya puchna hai?----------")
 while True:
     prompt = input("You: ")
-    messages.append({"role": "user", "content": prompt})
+    messages.append(HumanMessage(content=prompt))
     if prompt.lower() in ["exit", "quit"]:
         print(messages)
         print("Chalo bhaag jao! Bye!")
         break
     response = model.invoke(messages)
-    messages.append({"role": "assistant", "content": response.content})
+    messages.append(AIMessage(content=response.content))
     print(f"Gyani: {response.content}")    
