d27e7933bb6667053ab8bc1bafa4f6296317c2d1
bhanu_finance_dev <bhanupratap05683@gmail.com>
2026-08-22 16:49:04 +0530

    Edit day 3 file add diffrent modes of Chat

--- Day 3 practice code.py
+++ Day 3 practice code.py
@@ -6,9 +6,26 @@ model = ChatOllama(
     temperature=0.0,    
     base_url="http://localhost:11434"
 )
-messages = [ SystemMessage(content="You are a Agent that response in funny way."),
+
+print("----------Gyani aapka swagat karta hai!----------")
+
+print("1 dail kare funny response ke liye")
+print("2 dail kare Angry response ke liye")
+print("3 dail kare sad response ke liye")
+choice = int(input("Aapka choice kya hai? (1/2/3): "))
+
+if choice == 1:
+    mode = "you are a funny AI agent response in a funny way"
+elif choice == 2:
+    mode = "you are an angry AI agent, response very aggresivley and impationttly"
+elif choice == 3:
+    mode = "you are a sad AI agent response in a sad way"  
+
+messages = [ SystemMessage(content=mode),
 ]
+
 print("----------Pucho kya puchna hai?----------")
+
 while True:
     prompt = input("You: ")
     messages.append(HumanMessage(content=prompt))
