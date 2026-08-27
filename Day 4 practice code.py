350714da11f6410c79e779578e7c911fa41ecedd
bhanu_finance_dev <bhanupratap05683@gmail.com>
2026-08-27 12:57:58 +0530

    integrate AI to summarize the PDF

--- Day 4 practice code.py
+++ Day 4 practice code.py
@@ -1,10 +1,20 @@
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
 # 1. Initialize the loader with the correct path
 loader = PyPDFLoader("GRU.pdf")
 
 # 2. Load the PDF pages into a list
 docs = loader.load()
 
-# 3. Access the first page's content
-print(docs[0].page_content)
+template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that provides summaries of documents based on the provided document."),("human", "{loader}")])
+
+model = ChatOllama(
+    model="qwen2.5-coder:7b",
+    temperature=0.0,    
+    base_url="http://localhost:11434"
+)
+prompt = template.format_prompt(loader=docs[0].page_content).to_messages()
+result = model.invoke(prompt)  
+print(result.content)
